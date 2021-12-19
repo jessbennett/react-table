63a08e7f77094ec6f512cb2c376048d0901e925f
Jess <jessbennett924@gmail.com>
2021-12-19 15:43:04 -0500

    added basic table

---
+++ src/components/Table.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+
+class Table extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      students: [
+        { id: 1, name: 'Wasif', age: 21, email: 'wasif@email.com' },
+        { id: 2, name: 'Ali', age: 19, email: 'ali@email.com' },
+        { id: 3, name: 'Saad', age: 16, email: 'saad@email.com' },
+        { id: 4, name: 'Asad', age: 25, email: 'asad@email.com' },
+        { id: 5, name: 'Wasif', age: 21, email: 'wasif@email.com' },
+        { id: 6, name: 'Ali', age: 19, email: 'ali@email.com' },
+        { id: 7, name: 'Saad', age: 16, email: 'saad@email.com' },
+        { id: 8, name: 'Asad', age: 25, email: 'asad@email.com' },
+      ],
+    };
+  }
+
+  renderTableHeader() {
+    let header = Object.keys(this.state.students[0]);
+    return header.map((key, index) => {
+      return <th key={index}>{key.toUpperCase()}</th>;
+    });
+  }
+
+  renderTableData() {
+    return this.state.students.map((student, index) => {
+      const { id, name, age, email } = student; //destructuring
+      return (
+        <tr key={id}>
+          <td>{id}</td>
+          <td>{name}</td>
+          <td>{age}</td>
+          <td>{email}</td>
+        </tr>
+      );
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1 id='title'>React Dynamic Table</h1>
+        <table id='students'>
+          <tbody>
+            <tr>{this.renderTableHeader()}</tr>
+            {this.renderTableData()}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+}
+
+export default Table;
