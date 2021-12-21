c1a5d5a8f5ad451ac3a25b93ad21cdcf93a2ae13
Jess <jessbennett924@gmail.com>
2021-12-21 16:17:36 -0500

    finished

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import Table from './components/Table';
+import InputForm from './components/InputForm';
+// import Table from './components/Table';
 
 import './styles.css';
 
 function App() {
-  return <Table />;
+  return (
+    <>
+      <InputForm />
+      {/* <Table /> */}
+    </>
+  );
 }
 
 export default App;
--- src/components/Table.js
+++ src/components/Table.js
@@ -1,56 +1,49 @@
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
 
-class Table extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      students: [
-        { id: 1, name: 'Wasif', age: 21, email: 'wasif@email.com' },
-        { id: 2, name: 'Ali', age: 19, email: 'ali@email.com' },
-        { id: 3, name: 'Saad', age: 16, email: 'saad@email.com' },
-        { id: 4, name: 'Asad', age: 25, email: 'asad@email.com' },
-        { id: 5, name: 'Wasif', age: 21, email: 'wasif@email.com' },
-        { id: 6, name: 'Ali', age: 19, email: 'ali@email.com' },
-        { id: 7, name: 'Saad', age: 16, email: 'saad@email.com' },
-        { id: 8, name: 'Asad', age: 25, email: 'asad@email.com' },
-      ],
-    };
-  }
+// class Table extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       music: [
+//         { id: 1, name: 'Wasif' },
+//         { id: 2, name: 'Ali' },
+//         { id: 3, name: 'Saad' },
+//       ],
+//     };
+//   }
 
-  renderTableHeader() {
-    let header = Object.keys(this.state.students[0]);
-    return header.map((key, index) => {
-      return <th key={index}>{key.toUpperCase()}</th>;
-    });
-  }
+//   renderTableHeader() {
+//     let header = Object.keys(this.state.music[0]);
+//     return header.map((key, index) => {
+//       return <th key={index}>{key.toUpperCase()}</th>;
+//     });
+//   }
 
-  renderTableData() {
-    return this.state.students.map((student, index) => {
-      const { id, name, age, email } = student; //destructuring
-      return (
-        <tr key={id}>
-          <td>{id}</td>
-          <td>{name}</td>
-          <td>{age}</td>
-          <td>{email}</td>
-        </tr>
-      );
-    });
-  }
+//   renderTableData() {
+//     return this.state.music.map((list, index) => {
+//       const { id, name } = list; //destructuring
+//       return (
+//         <tr key={id}>
+//           <td>{id}</td>
+//           <td>{name}</td>
+//         </tr>
+//       );
+//     });
+//   }
 
-  render() {
-    return (
-      <div>
-        <h1 id='title'>React Dynamic Table</h1>
-        <table id='students'>
-          <tbody>
-            <tr>{this.renderTableHeader()}</tr>
-            {this.renderTableData()}
-          </tbody>
-        </table>
-      </div>
-    );
-  }
-}
+//   render() {
+//     return (
+//       <div>
+//         <h1 id='title'>React Dynamic Table</h1>
+//         <table id='music'>
+//           <tbody>
+//             <tr>{this.renderTableHeader()}</tr>
+//             {this.renderTableData()}
+//           </tbody>
+//         </table>
+//       </div>
+//     );
+//   }
+// }
 
-export default Table;
+// export default Table;
